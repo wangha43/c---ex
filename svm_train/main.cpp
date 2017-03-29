eb15182f185e34dd9299ef2cc89b87162fb61ae0
wxc <wangha43@163.com>
2017-03-29 14:30:04 +0800

    add shared memory

---
+++ svm_train/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/ml/ml.hpp>
+using namespace std;
+using namespace cv;
+
+#define POSINUM 2000
+#define NEGANUM 12000
+#define TRAIN false
+#define CENTRAL_CROP true
+#define HardExampleNO 0
+
+
+
+int main()
+{
+
+return 0;
+}
+
